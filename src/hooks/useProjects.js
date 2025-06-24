55f2bca609d79f16fa17cc5ca7d37c4a0fcc4ffd
AlexRubenPumari <alexpumari0@gmail.com>
2025-06-24 10:38:51 -0300

    [Fix] project filters in useProject custom hook

--- src/hooks/useProjects.js
+++ src/hooks/useProjects.js
@@ -2,22 +2,26 @@ import { useState, useEffect } from 'react'
 import { getTitleFrom } from '../logic/data'
 import { getProjects } from '../services/projects'
 
-export function useProjects () {
+export function useProjects() {
   const [projects, setProjects] = useState([])
-  const mappedProjects = projects.map(({id, description, topics, name, html_url, homepage}) => ({
-    id: id,
-    title: getTitleFrom(topics.find(topic => topic[0] === '1')),
-    description: description,
-    imgUrl: `https://raw.githubusercontent.com/AlexRubenPumari/${name}/master/cover.jpg`,
-    tags: topics.filter(topic => topic[0] !== '1'),
-    repoUrl: html_url,
-    pageUrl: homepage,
-  }))
+  const mappedProjects = 
+    projects
+      ?.filter(({ description, topics }) => description && topics)
+      ?.map(({ id, description, topics, name, html_url, homepage }) => (
+        {
+          id: id,
+          title: getTitleFrom(topics.find(topic => topic[0] === '1')),
+          description: description,
+          imgUrl: `https://raw.githubusercontent.com/AlexRubenPumari/${name}/master/cover.jpg`,
+          tags: topics.filter(topic => topic[0] !== '1'),
+          repoUrl: html_url,
+          pageUrl: homepage,
+        }
+      ))
   useEffect(() => {
     getProjects()
       .then(projects => setProjects(projects))
       .catch(error => console.log(error))
   }, [])
-  
   return { projects: mappedProjects }
 }
