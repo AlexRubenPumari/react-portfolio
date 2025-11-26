b02fb6f2982ea076b1ab4098fdb1f2120f845ce3
Alex Pumari <alexpumari0@gmail.com>
2025-11-26 15:33:06 -0300

    [Feat] add flaps component and stories

---
+++ src/components/interface/Flaps/flaps.tsx
@@ -0,0 +1,30 @@
+import { Flap } from "../flap/flap.js"
+import type { FlapValue } from "../flap/flap.js"
+import "./flaps.scss"
+
+interface Flap {
+  value: FlapValue
+  onClick?: (value: FlapValue) => void
+}
+
+interface FlapsProps {
+  flaps: Flap[]
+  selectedFlap: FlapValue
+}
+
+export function Flaps({ flaps, selectedFlap } : FlapsProps) {
+  return (
+    <>
+      <ul className="flaps">
+        {flaps.map(({ value, onClick }: Flap) => (
+          <Flap
+            key={value}
+            value={value}
+            isSelectedFlap={value === selectedFlap}
+            {...(onClick ? { onClick } : {})}
+          />
+        ))}
+      </ul>
+    </> 
+  )
+}
