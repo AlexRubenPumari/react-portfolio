739a9fae9431c7209fb9605a8d74ec8f6fc435ca
alex <alexpumari0@gmail.com>
2025-12-04 14:05:54 -0300

    refactor: improve generic typing

--- src/components/interface/menu/item.tsx
+++ src/components/interface/menu/item.tsx
@@ -1,20 +1,13 @@
 import { joinClasses } from "../../../logic/index.js"
 import "./item.scss"
 
-export interface Item {
-  value: ItemValue
-  onClick?: (value: ItemValue) => void
-}
-
-export type ItemValue = string | number
-
-interface ItemProps {
-  value: ItemValue
+interface ItemProps<ItemType> {
+  value: ItemType
   selected: boolean
-  onClick?: (value: ItemValue) => void
+  onClick?: (value: ItemType) => void
 }
 
-export function Item ({ value, selected, onClick }: ItemProps) {
+export function Item<ItemType extends string | number> ({ value, selected, onClick }: ItemProps<ItemType>) {
   const itemClasses = joinClasses(
     "item",
     selected && "item--selected"
