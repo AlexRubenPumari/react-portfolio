a66549c9df512e99381a2e0c38b3cae7016b4f1b
alex <alexpumari0@gmail.com>
2025-12-06 17:28:12 -0300

    refactor: delete use-modal, use-page, use-stepper, use-toggler custom hooks

--- src/hooks/useModal.js
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react'
-
-export default function useModal (initialModal) {
-  const [modal, setModal] = useState(initialModal)
-  const closeModal = () => setModal(null)
-
-  return { modal, setModal, closeModal }
-}
