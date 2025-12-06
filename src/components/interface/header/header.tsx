37a1bb1653ef072e9bfc7a2f6f9e69f1af58f128
alex <alexpumari0@gmail.com>
2025-12-06 17:37:00 -0300

    refactor: change header component and styles

--- src/components/interface/header/header.tsx
+++ src/components/interface/header/header.tsx
@@ -1,71 +1,64 @@
-// import { usePagesContext } from "../../../contexts/pages.jsx"
+import { useState } from "react"
+import { usePageContext } from "../../../contexts/page.js"
 import { cvIcon, githubIcon, linkedinIcon, portfolioIcon } from "../../../assets/img/index.js"
 import { Button, Flaps, MenuButton } from "../index.js"
+import { pageNames } from "../../../config/constants.js"
+import { getPage, getPageName } from "../../../logic/index.js"
 import cv from "../../../assets/pdf/cv.pdf"
 import "./header.scss"
-import { useState } from "react"
 
-const buttons = [
-  {
-    label: "Ver CV",
-    imgPath: cvIcon,
-    onClick: () => window.open(cv, "_blank"),
-  },
-  {
-    label: "Ver Proyectos",
-    imgPath: portfolioIcon,
-    useContextAction: true,
-  },
-  {
-    label: "Mi GitHub",
-    imgPath: githubIcon,
-    onClick: () => window.open("https://github.com/AlexRubenPumari", "_blank"),
-  },
-  {
-    label: "Mi LinkedIn",
-    imgPath: linkedinIcon,
-    onClick: () => window.open("https://www.linkedin.com/in/alex-pumari-diaz/", "_blank"),
-  },
-]
-
-const pageNames = ["Inicio", "Proyectos", "Sobre mi", "Contacto"] as const
-type PageName = typeof pageNames[number]
-function getPageName(page: number): PageName {
-  return pageNames[page - 1]!
-}
-function getPage(pageName: PageName): number {
-  const page = pageNames.findIndex(currentPageName => currentPageName === pageName) + 1
-  return page
-}
 const themes = ["Light", "Dark"] as const
 type Theme = typeof themes[number]
 
 export function Header () {
-  // const { goToFirstProjectPage } = usePagesContext()
-  const [page, setPage] = useState<number>(1)
+  const { page, setPage } = usePageContext()
   const [theme, setTheme] = useState<Theme>("Light")
+
+  const buttons = [
+    {
+      label: "Ver CV",
+      imgPath: cvIcon,
+      onClick: () => window.open(cv, "_blank"),
+    },
+    {
+      label: "Ver Proyectos",
+      imgPath: portfolioIcon,
+      onClick: () => setPage(3),
+    },
+    {
+      label: "Mi GitHub",
+      imgPath: githubIcon,
+      onClick: () => window.open("https://github.com/AlexRubenPumari", "_blank"),
+    },
+    {
+      label: "Mi LinkedIn",
+      imgPath: linkedinIcon,
+      onClick: () => window.open("https://www.linkedin.com/in/alex-pumari-diaz/", "_blank"),
+    },
+  ]
 
   return (
     <header className="header">
       <nav className="header__nav">
         <Flaps
-          flaps={pageNames as unknown as string[]}
+          flaps={pageNames}
           selectedFlap={getPageName(page)}
-          onChange={pageName => setPage(getPage(pageName as PageName))}
+          onChange={pageName => setPage(getPage(pageName))}
         />
         <MenuButton
           label="Theme"
-          items={themes as unknown as string[]}
+          items={themes}
           direction="bottom-right"
           selectedItem={theme}
-          onChange={theme => setTheme(theme as Theme)}
+          onChange={theme => setTheme(theme)}
         />
       </nav>
       <div className="header__panel">
         <div className="header__actions">
-        {buttons.map(({ label, imgPath, onClick, useContextAction }) => (
+        {buttons.map(({ label, imgPath, onClick }) => (
           <Button
             direction="column"
+            onClick={onClick}
             key={label}
           >
             <img className="icon icon--md" src={imgPath} alt="icon" />
