714bd77321da00097cb6f84cbfb8a2e097afc34c
alex <alexpumari0@gmail.com>
2025-12-08 00:17:41 -0300

    feat: add subtitle component and style

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -1,2 +1,6 @@
 export * from "./blob/index.js"
 export * from "./button/index.js"
+export * from "./subtitle/index.js"
+
+export * from "./about-me-page/about-me-page.js"
+export * from "./index-page/index-page.js"
