8d0c914ffd8fada9c09e81758907a196579f02e2
alex <alexpumari0@gmail.com>
2025-12-08 11:59:20 -0300

    refactor: rewrite circle-pattern to pattern component

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -1,6 +1,8 @@
-export * from "./blob/index.js"
-export * from "./button/index.js"
-export * from "./subtitle/index.js"
+export * from "./blob/blob.js"
+export * from "./button/button.js"
+export * from "./subtitle/subtitle.js"
+export * from "./pattern/pattern.js"
 
 export * from "./about-me-page/about-me-page.js"
 export * from "./index-page/index-page.js"
+export * from "./contact-page/contact-page.js"
