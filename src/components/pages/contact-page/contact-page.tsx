ccd735ce5d23315cdd4e6c9e692f05a7e1ee8009
alex <alexpumari0@gmail.com>
2025-12-08 12:01:02 -0300

    refactor: rewrite contact-page component

---
+++ src/components/pages/contact-page/contact-page.tsx
@@ -0,0 +1,28 @@
+import { GithubIcon, LinkedinIcon } from "../../icons/index.js"
+import { Blob, Button, Pattern } from "../index.js"
+import "./contact-page.scss"
+
+export function ContactPage() {
+  return (
+    <div className="contact-page">
+      <h2 className="contact-page__title">¡Quedo a la espera de tu mensaje!</h2>
+      <div className="contact-page__buttons">
+        <Button onClick={() => window.open("https://github.com/AlexRubenPumari", "_blank")}>
+          <GithubIcon size="lg" />
+          <span>Mi Github</span>
+        </Button>
+        <Button onClick={() => window.open("https://www.linkedin.com/in/alex-pumari-diaz/", "_blank")}>
+          <LinkedinIcon size="lg" />
+          <span>Mi LinkedIn</span>
+        </Button>
+      </div>
+      <Blob shape="b" className="contact-page__blob" />
+      <Pattern
+        shape="circle"
+        rows={1}
+        cols={4}
+        className="contact-page__pattern"
+      />
+    </div>
+  )
+}
