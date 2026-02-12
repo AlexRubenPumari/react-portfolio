0d7324d0f21e9f506d05685be496cca8fc1f989e
alex <alexpumari0@gmail.com>
2026-02-11 21:14:53 -0300

    refactor: move zoom-values constant in config module

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,3 @@
 export const pageNames = ["Inicio", "Sobre mi", "Proyectos", "Contacto"] as const
-
 export const themes = ["Light", "Dark"] as const
+export const zoomValues = [60, 80, 100, 120, 140] as const
