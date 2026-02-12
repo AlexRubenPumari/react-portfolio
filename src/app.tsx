46051537be5d628e36d11ce6cbdaf4645862c58f
alex <alexpumari0@gmail.com>
2026-02-11 21:16:54 -0300

    feat: use zoom context in app component

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,20 @@
 import { useState } from "react"
 import { Header, Footer, Viewport } from "./components/interface/index.js"
-import { PageContext } from "./contexts/page.js"
-import type { Page } from "./types/page.js"
+import { PageContext, ZoomContext } from "./contexts/index.js"
+import type { Page, ZoomValue } from "./types/index.js"
 import "./styles/index.scss"
 
 export default function App() {
   const [page, setPage] = useState<Page>(1)
+  const [zoom, setZoom] = useState<ZoomValue>(100)
 
   return (
     <PageContext.Provider value={{ page, setPage }}>
-      <Header />
-      <Viewport page={page} />
-      <Footer />
+      <ZoomContext.Provider value={{ zoom, setZoom }}>
+        <Header />
+        <Viewport page={page} />
+        <Footer />
+      </ZoomContext.Provider>
     </PageContext.Provider>
   )
 }
