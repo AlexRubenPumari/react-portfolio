946acaa86a4a6b69b0a853c659c0b8ccfc8fbd03
alex <alexpumari0@gmail.com>
2026-03-04 17:46:43 -0500

    refactor: change logic and add util in decode-content file

--- src/logic/decoder-content.test.ts
+++ src/logic/decoder-content.test.ts
@@ -3,7 +3,7 @@ import { decode } from "./decode-content.js"
 
 describe("decoder-base64", () => {
   test("should decode a valid base64 string", () => {
-    const base64Content: string = "SGVsbG8=" // "Hello"
+    const base64Content: string = "SGVsbG8=" // "Hello" in base-64 string
 
     const result = decode.base64(base64Content)
 
@@ -23,11 +23,11 @@ describe("decoder-base64", () => {
   })
 })
 
-describe("decoder-utf8", () => {
+describe("decoder-url", () => {
   test("should decode a valid UTF-8 escaped string", () => {
     const utfContent = "Hello%20World"
 
-    const result = decode.utf8(utfContent)
+    const result = decode.url(utfContent)
 
     expect(result).toBe("Hello World")
   })
@@ -35,14 +35,25 @@ describe("decoder-utf8", () => {
   test("should return original value when decoding fails", () => {
     const invalidContentURISequence = "%E0%A4%A"
 
-    const result = decode.utf8(invalidContentURISequence)
+    const result = decode.url(invalidContentURISequence)
 
     expect(result).toBe(invalidContentURISequence)
   })
 
   test("should return empty string when input is empty", () => {
-    const result = decode.utf8("")
+    const result = decode.url("")
 
     expect(result).toBe("")
+  })
+})
+
+describe("decoder-utf8", () => {
+  test("should decode a string of utf-8 bytes", () => {
+    const bytesStr = "\xF0\x9F\x98\x80" // 😀 in utf-8 bytes
+    expect(decode.utf8(bytesStr)).toBe("😀")
+  })
+
+  test("should return empty string when input is empty", () => {
+    expect(decode.utf8("")).toBe("")
   })
 })
