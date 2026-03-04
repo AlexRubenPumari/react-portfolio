3c6d80b4aab31be81906402112eafed3582a74a5
alex <alexpumari0@gmail.com>
2026-03-04 18:18:12 -0500

    refactor: add fetch options in fetch-json util

--- src/services/fetch-json.ts
+++ src/services/fetch-json.ts
@@ -1,5 +1,9 @@
-export async function fetchJson<Type>(url: string): Promise<Type> {
-  const response = await fetch(url)
+interface FetchOptions {
+  headers?: Record<string, string>,
+}
+
+export async function fetchJson<Type>(url: string, options: FetchOptions = {}): Promise<Type> {
+  const response = await fetch(url, options)
 
   if (!response.ok) throw new Error(`Request failed: ${response.status}`)
 
--- src/services/github-service.ts
+++ src/services/github-service.ts
@@ -2,13 +2,16 @@ import { formatRepositories } from "../adapters/repositories.js"
 import { decode } from "../logic/index.js"
 import { fetchJson } from "./fetch-json.js"
 import type { GitHubRepository, GithubService, Repository } from "../types/index.js"
+import { GITHUB_API_BASE, GITHUB_TOKEN, REPOSITORIES_URL } from "../config/constants.js"
 
-const GITHUB_API_BASE = "https://api.github.com"
-const REPOSITORIES_URL = `${GITHUB_API_BASE}/users/alexrubenpumari/repos`
+const fetchOptions = {
+  headers: { "Authorization": `token ${GITHUB_TOKEN}` }
+}
 
 export const githubService: GithubService = {
+
   getRepositories(): Promise<Repository[]> {
-    return fetchJson<GitHubRepository[]>(REPOSITORIES_URL)
+    return fetchJson<GitHubRepository[]>(REPOSITORIES_URL, fetchOptions)
       .then(repositories => formatRepositories(repositories))
   },
 
@@ -17,11 +20,12 @@ export const githubService: GithubService = {
   
     const url = `${GITHUB_API_BASE}/repos/${repositoryFullName}/readme`
   
-    return fetchJson<RepositoryReadme>(url)
+    return fetchJson<RepositoryReadme>(url, fetchOptions)
       .then(response => {
         if (!response.content) throw new Error("README content not found.")
-  
-        return decode.base64(response.content)
+        const decodedReadmeContent = decode.utf8(decode.base64(response.content))
+
+        return decodedReadmeContent
       })
   }
 }
