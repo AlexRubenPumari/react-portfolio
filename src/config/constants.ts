43c42ad518e9f9746c84612c6c0f4c7f78ac2155
alex <alexpumari0@gmail.com>
2026-03-20 20:15:27 -0400

    feat: add build-repository-image-url util an tests

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,6 +2,8 @@ export const PAGE_NAMES = ["Inicio", "Sobre mi", "Proyectos", "Contacto"] as con
 export const THEMES = ["Light", "Dark"] as const
 export const ZOOM_VALUES = [60, 80, 100, 120, 140] as const
 
+export const GITHUB_USERNAME = "AlexRubenPumari"
 export const GITHUB_API_BASE = "https://api.github.com"
-export const REPOSITORIES_URL = `${GITHUB_API_BASE}/users/alexrubenpumari/repos`
+export const REPOSITORIES_URL = `${GITHUB_API_BASE}/users/${GITHUB_USERNAME}/repos`
 export const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN
+export const GITHUB_IMAGE_BASE_URL = `https://raw.githubusercontent.com`
