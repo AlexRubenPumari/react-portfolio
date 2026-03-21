bdc2f56bae887c1bf4f55f9aafc239f6f63e4791
alex <alexpumari0@gmail.com>
2026-03-20 20:18:42 -0400

    refactor: rewrite get-repository-details use case

--- src/use-cases/get-repository-details.ts
+++ src/use-cases/get-repository-details.ts
@@ -1,29 +1,33 @@
-import { parseRepositoryReadmeContent } from "../logic/repository-readme-content/parse-repository-readme-content.js"
+import { parseRepositoryReadmeContent, buildRepositoryImageUrl } from "../logic/index.js"
 import type { GithubService, RepositoryDetails, RepositoryReadme } from "../types/index.js"
 
 export async function getRepositoriesDetails(
-  { githubService } : {  githubService: GithubService }
+  { githubService }: { githubService: GithubService }
 ): Promise<RepositoryDetails[]> {
   const repositories = await githubService.getRepositories()
 
   const results = await Promise.all(
     repositories.map(async repository => {
       const readmeContent = await githubService.getRepositoryReadmeContent(repository.fullName)
-      const repositoryReadme: RepositoryReadme = parseRepositoryReadmeContent(readmeContent)
+      const { title, badges, imagePath, videoUrl, learnings }: RepositoryReadme = parseRepositoryReadmeContent(readmeContent)
+      const imageUrl = imagePath
+        ? buildRepositoryImageUrl({ fullName: repository.fullName, imagePath })
+        : null
 
-      return { ...repository, ...repositoryReadme } satisfies RepositoryDetails
+      return { ...repository, title, badges, videoUrl, learnings, imageUrl }
     })
   )
 
-  const validRepositories: RepositoryDetails[] = results.filter(isValidRepository)
+  const validResults: RepositoryDetails[] = results.filter(isValidRepositoryDetails)
 
-  return validRepositories
+  return validResults
 }
 
-function isValidRepository ({ id, title, description }: RepositoryDetails): boolean {
+function isValidRepositoryDetails({ id, title, description, imageUrl }: RepositoryDetails): boolean {
   const PORTFOLIO_REPOSITORY_ID = 976695014
   const PERSONAL_REPOSITORY_ID = 1019148150
-  const repositoryHasValidData = title !== null && description !== null
+  const repositoryHasValidData = title !== null && description !== null && imageUrl !== null
 
   return id !== PERSONAL_REPOSITORY_ID && id !== PORTFOLIO_REPOSITORY_ID && repositoryHasValidData
 }
+
